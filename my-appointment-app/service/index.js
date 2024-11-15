c7bcde6b11e2ee18153d5225db4c0475a4b64feb
MariaF7117 <mgf11@byu.edu>
2024-11-15 14:04:32 -0700

    edit routes in indes.js

--- my-appointment-app/service/index.js
+++ my-appointment-app/service/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const uuid = require('uuid');
+const { default: AppointmentList } = require('../src/appointmentList/appointmentList');
 const app = express();
 
 // The scores and users are saved in memory and disappear whenever the service is restarted.
@@ -53,15 +54,15 @@ apiRouter.delete('/auth/logout', (req, res) => {
   res.status(204).end();
 });
 
-// GetScores
-apiRouter.get('/scores', (_req, res) => {
-  res.send(scores);
+// register
+apiRouter.get('/register', (_req, res) => {
+  res.send(register);
 });
 
-// SubmitScore
-apiRouter.post('/score', (req, res) => {
-  scores = updateScores(req.body, scores);
-  res.send(scores);
+// scheduleAppointment
+apiRouter.post('/schedule', (req, res) => {
+  schedule = updateSchedule(req.body, schedule);
+  res.send(schedule);
 });
 
 // Return the application's default page if the path is unknown
@@ -69,28 +70,10 @@ app.use((_req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
 
+appRouter.get('appointmentList', (_req,res) => {
+    res.send(AppointmentList)
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
-// updateScores considers a new score for inclusion in the high scores.
-function updateScores(newScore, scores) {
-  let found = false;
-  for (const [i, prevScore] of scores.entries()) {
-    if (newScore.score > prevScore.score) {
-      scores.splice(i, 0, newScore);
-      found = true;
-      break;
-    }
-  }
-
-  if (!found) {
-    scores.push(newScore);
-  }
-
-  if (scores.length > 10) {
-    scores.length = 10;
-  }
-
-  return scores;
-}
