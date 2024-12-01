4dfe4d1176a636a1f34ab2d4dc59b61a5a5f9282
MariaF7117 <mgf11@byu.edu>
2024-12-01 11:39:55 -0700

    figureing out login

--- my-appointment-app/src/login/login.jsx
+++ my-appointment-app/src/login/login.jsx
@@ -1,64 +1,30 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+
 import '../App.css';
+import React from 'react';
 
-const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate();
+import { Unauthenticated } from './unauthenticated';
+import { Authenticated } from './authenticated';
+import { AuthState } from './authState';
 
-  const loginUser = (event) => {
-    event.preventDefault();
-
-    // Retrieve users from localStorage
-    const users = JSON.parse(localStorage.getItem('users')) || [];
-
-    // Check if the entered credentials match any registered user
-    const user = users.find(
-      (user) => user.username === username && user.password === password
-    );
-
-    if (user) {
-      alert('Login successful!');
-      // Redirect to a protected page or dashboard
-      navigate('/'); // Update path as needed
-    } else {
-      alert('Invalid username or password. Please try again.');
-    }
-  };
-
+export function Login({ userName, authState, onAuthChange }) {
   return (
-    <main>
-      <div className="login-container">
-        <h1>Login</h1>
-        <form onSubmit={loginUser}>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
+    <main className='container-fluid bg-secondary text-center'>
+      <div>
+        {authState !== AuthState.Unknown && <h1>Welcome to Simon</h1>}
+        {authState === AuthState.Authenticated && (
+          <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+        )}
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+            userName={userName}
+            onLogin={(loginUserName) => {
+              onAuthChange(loginUserName, AuthState.Authenticated);
+            }}
           />
-
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-
-          <button type="submit">Login</button>
-        </form>
-        <br />
-        <button onClick={() => navigate('/register')}>Register</button>
+        )}
       </div>
     </main>
   );
-};
+}
 
 export default Login;
