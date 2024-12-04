08d0c17de1575cbf5121d965681353fc68729202
MariaF7117 <mgf11@byu.edu>
2024-12-04 15:51:42 -0700

    authenticated

--- my-appointment-app/src/login/authenticated.jsx
+++ my-appointment-app/src/login/authenticated.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../App.css';
 
+import Button from 'react-bootstrap/Button';
+
+import './authenticated.css';
 
 export function Authenticated(props) {
   const navigate = useNavigate();
